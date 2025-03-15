9ba43fde3f55ee3783ef9eaf872e2f51a252a90b
eiccin <idanandreip@gmail.com>
2025-03-15 01:53:44 +0100

    add sidebar and theme support for core ui

---
+++ components/app-sidebar.tsx
@@ -0,0 +1,20 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+} from "@/components/ui/sidebar";
+
+export function AppSidebar() {
+  return (
+    <Sidebar className="bg-gray-700">
+      <SidebarHeader />
+      <SidebarContent>
+        <SidebarGroup></SidebarGroup>
+        <SidebarGroup />
+      </SidebarContent>
+      <SidebarFooter />
+    </Sidebar>
+  );
+}
